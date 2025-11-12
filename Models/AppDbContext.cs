428b236bd097bb6b0005472efaf97c7a61e5c197
James1020-world <JamesScott.1020@outlook.com>
2025-11-12 03:47:50 -0800

    Add files via upload

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AppointmentHubMVC.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Appointments> Appointments { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected AppDbContext()
+        {
+        }
+    }
+}
---
+++ Models/Appointments.cs
@@ -0,0 +1,15 @@
+﻿namespace AppointmentHubMVC.Models
+{
+    public class Appointments
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateOnly? EndDate { get; set;}
+
+        public int Status { get; set; }
+    }
+}
